5253b50fa08b708c8b1e8d78f1f9e5df30b78a4d
tianscar <tianscar@protonmail.com>
2023-05-02 15:46:01 +0800

    Site updated: 2023-05-02 15:46:01

--- android-bmp-encoder/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"com.tianscar.android.graphics","c":"BMPEncoder","l":"compress(Bitmap, OutputStream)","url":"compress(android.graphics.Bitmap,java.io.OutputStream)"}]
--- colorful-xcursor/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.tianscar.awt.x11","l":"ColorfulXCursor"}];updateSearchResults();
