f2ea1d98c736f96d14879cf2fdf0b4ae26d50c8a
tianscar <tianscar@protonmail.com>
2023-05-02 20:24:21 +0800

    Site updated: 2023-05-02 20:24:21

--- mkindex.sh
+++ mkindex.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-tree -H '.' -L 1 --noreport -d -T 'docs.tianscar.com' -I ".*" --charset utf-8 | sed -e '/<\/*meta name=.*>/d' | sed -e '/<p class=\"VERSION\">/,+5d' | sed -e '/<hr>/a\\t<p class="VERSION">\n\t\t2022 - 2023 (c) Karstian Lee\n\t<\/p>' > index.html
+tree -H '.' -L 1 --noreport -d -T '碳酸天剑的文档' -I ".*" --charset utf-8 | sed -e '/<\/*meta name=.*>/d' | sed -e '/<p class=\"VERSION\">/,+5d' | sed -e '/<hr>/a\\t<p class="VERSION">\n\t\t2022 - 2023 (c) Karstian Lee\n\t<\/p>' > index.html
